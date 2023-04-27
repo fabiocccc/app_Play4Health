afe2ce15bf3c9f14ca8681c1072f95076e53996b
fratravisani <f.travisani@studenti.uniba.it>
2023-04-27 12:40:20 +0200

    Aggiunto Passo5 del Percorso2

--- app/src/main/java/com/example/tesi/AppTravisani/P2Passi/Passo5P2.java
+++ app/src/main/java/com/example/tesi/AppTravisani/P2Passi/Passo5P2.java
@@ -4,11 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.tesi.R;
+
 public class Passo5P2 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_passo5_p2);
+
+
+
     }
 }
